8e34ffe033eefd863651132d19877e968aea077e
VikashPalani <vikash1702palani@gmail.com>
2025-02-15 14:55:17 +0530

    App Layout and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,52 @@
+import {Outlet, RouterProvider, createBrowserRouter} from 'react-router-dom';
+
+//components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+//pages
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import StudentDashboard from './pages/StudentDashboard';
+
+const AppLayout = () => {
+  return (
+    <div className='flex flex-col min-h-screen bg-gray-100'>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
 const App = () => {
 
-  return (
-    <>
-      <h1 className="underline text-xl">StudySmart AI</h1>
-    </>
-  )
+  const appRouter = createBrowserRouter([
+    {
+      path:"/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: '/',
+          element: <Login />,
+        },
+        {
+          path: '/login',
+          element: <Login />,
+        },
+        {
+          path: '/signup',
+          element: <Signup />,
+        },
+        {
+          path: '/student-dashboard',
+          element: <StudentDashboard />,
+        },
+      ]
+    }
+  ]);
+
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
